2c765c7a6d714682b315b4aeaa0ea840583e3654
Yuri <yuri.santos@ccc.ufcg.edu.br>
2021-03-01 19:35:41 -0300

    configurando lint

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   res.send(`API rodando em ${process.env.BASE_URL || 3000}`);
 });
 
