7a33af8f6cf40267fab3bbeb2a8827db0e05f1c6
Yuri <yuri.santos@ccc.ufcg.edu.br>
2021-03-01 19:31:33 -0300

    Organização das pastas

---
+++ src/config/database.js
@@ -0,0 +1,51 @@
+require('dotenv/config');
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+    },
+    options: {
+      dialect: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+      timezone: '-03:00'
+    }
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    operatorsAliases: false,
+    options: {
+      dialect: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+      },
+      timezone: '-03:00'
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(`API rodando em ${process.env.BASE_URL || 3000}`);
-  });
+  res.send(`API rodando em ${process.env.BASE_URL || 3000}`);
+});
 
-  module.exports = router;
+module.exports = router;
